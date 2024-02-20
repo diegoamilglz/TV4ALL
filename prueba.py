1ab70382c54880b1737d6de398a7efc6f6a7abee
Diego Amil González <diegoamilglz@gmail.com>
2024-02-20 23:24:13 +0100

    prueba mesh con fps en pantalla

--- prueba.py
+++ prueba.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import time
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -7,7 +8,9 @@ mp_face_mesh = mp.solutions.face_mesh
 
 # For webcam input:
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+fps_timer = 0
+fps = 0
 with mp_face_mesh.FaceMesh(
     max_num_faces=1,
     static_image_mode=False,
@@ -55,8 +58,15 @@ with mp_face_mesh.FaceMesh(
             connection_drawing_spec=mp_drawing_styles
             .get_default_face_mesh_iris_connections_style())
     # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Face Mesh', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
+    frame = cv2.flip(image, 1)
+    fps_end = time.time()   
+    time_diff = fps_end - fps_timer
+    fps = 1/(time_diff)
+    fps_timer = fps_end
+    fps_text = "FPS: {:.2f}".format(fps)
+    cv2.putText(frame, fps_text, (5,30),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 1)
+    cv2.imshow('MediaPipe Face Mesh', frame)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
       break
     
     
