55d285bf2b027d60c3694d90fb96f2f0438165cc
diegoamilglz <58148836+diegoamilglz@users.noreply.github.com>
2024-04-03 12:14:28 +0200

    bien

--- detectParpadeo.py
+++ detectParpadeo.py
@@ -6,14 +6,13 @@ from collections import deque
 import time
 #from findpeaks import findpeaks
 from scipy.signal import find_peaks
+from scipy.signal import medfilt
+
+
 
 
 def drawing_output(frame, coordinates_left_eye, coordinates_right_eye, blink_counter,fpsdisp):
      aux_image = np.zeros(frame.shape, np.uint8)
-     # contours1 = np.array([coordinates_left_eye])
-     # contours2 = np.array([coordinates_right_eye])
-     # cv2.fillPoly(aux_image, pts=[contours1], color=(128, 255, 200))
-     # cv2.fillPoly(aux_image, pts=[contours2], color=(128, 255, 200))
      output = cv2.addWeighted(frame, 1, aux_image, 0.7, 1)
      cv2.rectangle(output, (0, 0), (200, 50), (255, 0, 0), -1)
      cv2.rectangle(output, (202, 0), (265, 50), (255, 0, 0),2)
@@ -30,27 +29,6 @@ def eye_aspect_ratio(coordinates):
      #print("Distancia horizontal:", d_C)
      return (d_A + d_B) / (2 * d_C), d_C
 
-
-def plotting_ear(pts_ear, line1):
-     global figure
-     pts = np.linspace(0, 1, 64)
-     if line1 == []:
-          plt.style.use("ggplot")
-          plt.ion()
-          figure, ax = plt.subplots()
-          line1, = ax.plot(pts, pts_ear)
-          plt.ylim(0.1, 0.4)
-          plt.xlim(0, 1)
-          plt.ylabel("EAR", fontsize=18)
-     else:
-          line1.set_ydata(pts_ear)
-          figure.canvas.draw()
-          figure.canvas.flush_events()
-          if cv2.waitKey(5) & 0xFF == 27:
-               cv2.destroyAllWindows()
-     return line1
-            
-     
 def calculate_threshold(d_C):
      """
      Calculate the EAR threshold based on the horizontal distance of the eye in pixels (d_C).
@@ -70,30 +48,74 @@ def calculate_threshold(d_C):
 
      return EAR_THRESH
 
+def rising_edge(data, thresh):
+     sign = data >= thresh
+     pos = np.where(np.convolve(sign, [1, -1]) == 1)
+     return pos
+
+def representar_grafico(pts_ear_array, pts_ear_array_smooth, line1, line2):
+     # Create a new figure and two subplots: one for the original data and one for the smoothed data
+     global figure
+     pts_or = np.linspace(0, 1, 64)
+     pts_smooth = np.linspace(0, 1, len(pts_ear_array_smooth))
+     # pts_ear_array = pts_ear_array.flatten()
+     # pts_ear_array_smooth = pts_ear_array_smooth.flatten()
+     if line1 == []:
+          plt.style.use("ggplot")
+          plt.ion()
+          figure, (ax1, ax2) = plt.subplots(2, 1)
+          #line1, = ax1.plot([], [], 'b-', label='original data')
+          line1, = ax1.plot(pts_or, pts_ear_array, 'b-', label='original data')
+          #line2, = ax2.plot([], [], 'r-', label='smoothed data')
+          line2, = ax2.plot(pts_smooth, pts_ear_array_smooth, 'r-', label='smoothed data')
+          # Add a legend to each subplot
+          ax1.legend()
+          ax2.legend()
+          ax1.set_xlim(0, 1)
+          ax1.set_ylim(-0.45, -0.1)
+          ax2.set_xlim(0, 1)
+          ax2.set_ylim(-0.45, -0.1)
+     else:
+          line1.set_ydata(pts_ear_array)
+          line2.set_ydata(pts_ear_array_smooth)
+          figure.canvas.draw()
+          figure.canvas.flush_events()
+          if cv2.waitKey(5) & 0xFF == 27:
+               cv2.destroyAllWindows()
+
+     return line1, line2
+
+
 # Inicializa la captura de video
 
 cap = cv2.VideoCapture(0) #distancia máxima 1.80 metros mao meno
-# cap.set(3,1280)
-# cap.set(4,720)
+
+# DEFINIENDO LAS VARIABLES QUE SE UTILIZARÁN 
 # Definir el máximo número de valores que deseas almacenar
 MAX_VALORES = 1000
 mp_face_mesh = mp.solutions.face_mesh
 index_left_eye = [33,246,161,160,159,158,157,173,133,155,154,153,145,144,163,7] #[33, 160, 158, 133, 153, 144] Simpler version
 index_right_eye = [362,398,384,385,386,387,388,466,263,249,390,373,374,380,381,382] #[362, 385, 387, 263, 373, 380] Simpler version
 #EAR_THRESH = 0.27 #variable, si está mas cerca o mas lejos deberia variar 0.19
+# Definir el número de frames consecutivos que deben tener un valor de EAR inferior a EAR_THRESH para considerar que un usuario está parpadeando de forma intencional
 NUM_FRAMES = 15
 aux_counter = 0
 blink_counter = 0
+# Inicializa una lista vacía para almacenar los valores de ear
 line1 = []
+line2 = []
 pts_ear = deque(maxlen=64)
 i = 0
 a=0
+# Parámetros para mostrar los FPS en pantalla
 fc=0
 display_time=3
 FPS=0
 start_time=time.time()
-# Inicializa una lista vacía para almacenar los valores de ear
-#ear_values = deque(maxlen=1000)
+# Parámetros para suavizado de la señal
+window_size = 5
+# Parámetros para representar gráficos
+peak_detected = False
 
 with mp_face_mesh.FaceMesh(
      static_image_mode=False,
@@ -175,39 +197,41 @@ with mp_face_mesh.FaceMesh(
 
 
                #código usando find_peaks
-               pts_ear_array = np.array(pts_ear)
+               # Convierte la lista de valores de EAR en un array de numpy
+               pts_ear_array = -np.array(pts_ear)
+               
 
                # Espera a tener suficientes valores antes de buscar picos, para evitar falsos positivos
-               if len(pts_ear_array) > 63:
+               if len(pts_ear) > 63:
                     # Encuentra picos en los valores de EAR usando la función find_peaks
-                    line1 = plotting_ear(pts_ear, line1)
-                    peaks, _ = find_peaks(pts_ear_array, distance=10, width=1, prominence=0.1)#, wlen=15, width=15) #esto es lo que hay que modificar
-                    
+                    #line1 = plotting_ear(pts_ear, line1)
+                    pts_ear_array_smooth = medfilt(pts_ear_array, kernel_size=3)
+                    # Assume pts_ear_array_smooth is your smoothed data
+                    #window size usoo tamén para detectar un único pico que me interese
+                    pts_ear_array_smooth = np.convolve(pts_ear_array_smooth, np.ones(window_size)/window_size, mode='valid') 
+                    #line1 = plotting_ear(pts_ear_array_smooth, line1)
+                    #line1, line2 = representar_grafico(pts_ear_array, pts_ear_array_smooth, line1, line2) 
+                    # Display the plot
+                    peaks, diccionario = find_peaks(pts_ear_array_smooth, distance=10, prominence=0.038, width=10)#, wlen=15, width=15) #esto es lo que hay que modificar
                     # Imprime los índices de los picos encontrados (si hay alguno)
-                   
-                    if len(peaks) > 0:
-                         print("Picos encontrados en los índices:", peaks)
-                    else:
-                         print("No se encontraron picos")
-
-               # if a>200:
-               #      fp = findpeaks(method='peakdetect', lookahead=5)
-               #      results = fp.fit(pts_ear_array)
-               #      if results['df'] is not None:
-               #           peak_indices = results['df']['ix'].values
-               #           print("Peak indices:", peak_indices)
-               #      else:
-               #           print("No peaks found")
-               # a += 1
-
-
-               #código usando find_peaks
-               
-               # if a>150:
-               #      peaks, _ = find_peaks(pts_ear_array, distance=10)
-               #      print("Peaks:", peaks)
-               # a += 1
-
+                    if peaks.size > 0:
+                         if any(peaks>40) and not peak_detected:
+                         #     DIFERENTE PARA SIN GRÁFICAS
+                              # if not peak_detected and len(peaks) > 1:
+                              #      print("Varios picos consecutivos. peaks:", peaks)
+                              #      #podría ser necesario cambiarlo o tenerlo en cuenta
+                              # else:
+                              print("PICO DETECTADO")
+                              print("Picos encontrados en los índices:", peaks)
+                              print("Prominencias de los picos:", diccionario['prominences'])
+                              print("Bases izquierdas de los picos:", diccionario['left_bases'])
+                              print("Bases derechas de los picos:", diccionario['right_bases'], "\n")
+                              peak_detected = True
+                         elif peak_detected and any(peaks<=40):
+                              print(" RESETEO SEÑAL ENVIADA. Peaks: ", peaks)
+                              #aquí es simplemente para resetear el trigger de peak_detected
+                              peak_detected = False
+                         
 
           cv2.imshow("Frame", frame)
           if cv2.waitKey(1) & 0xFF == ord('q'):
